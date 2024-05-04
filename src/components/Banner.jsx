aac42ab1fb80010c00bf24987bf513d39c2c846c
Likhitha <likhitha077@gmail.com>
2024-05-05 03:04:18 +0530

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,10 @@
 export default function Banner() {
   return (
     <div
-      className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
+      className="h-[20vh] md:h-[50vh] bg-cover bg-center flex items-end"
       style={{
-        backgroundImage: `url(https://w0.peakpx.com/wallpaper/482/599/HD-wallpaper-dune-2021-movie.jpg)`,
+        backgroundImage: `url(https://cdn.marvel.com/content/1x/avengersendgamelive_lob_mas_dsk_01.jpg)`,
       }}
-    >
-      <div className="text-white text-2xl text-center w-full bg-gray-900/60 p-4">
-        Dune 2
-      </div>
-    </div>
+    ></div>
   );
 }
